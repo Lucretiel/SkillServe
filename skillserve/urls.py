e4a6a313b12a40e352eb162f0ca6b31063a3a342
Nathan West <Lucretiel@gmail.com>
2017-05-03 15:35:41 -0400

    typo

--- skillserve/urls.py
+++ skillserve/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from logging_tree import build_description
+from logging_tree.format import build_description
 
 from django.conf.urls import include
 from django.conf.urls import url
