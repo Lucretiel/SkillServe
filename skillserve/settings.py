ad68041da74ede9da8fe635ee2deedc9b08ab3e3
Nathan West <Lucretiel@gmail.com>
2017-05-02 20:09:01 -0400

    Added header middleware

--- skillserve/settings.py
+++ skillserve/settings.py
@@ -51,12 +51,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'skillboards.apps.SkillboardsConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
